00a922bc29bfd9dda1b246d8fc07671f3b9c0c28
Vladimir Sibirov <trustmaster@kodigy.com>
2012-03-26 22:43:27 +0400

    Moved to a separate repo

---
+++ view/lang/view.en.lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * English Language File for the View Module
+ *
+ * @package view
+ * @version 0.9.0
+ * @author Cotonti Team
+ * @copyright Copyright (c) Cotonti Team 2008-2012
+ * @license BSD
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$L['info_desc'] = 'Outputs static HTML or plain text body with common site header and footer';
+
+?>
---
+++ view/lang/view.ru.lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * English Language File for the View Module
+ *
+ * @package view
+ * @version 0.9.0
+ * @author Cotonti Team
+ * @copyright Copyright (c) Cotonti Team 2008-2012
+ * @license BSD
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$L['info_desc'] = 'Выводит статический HTML или чистый текст с общей для сайта шапкой и подвалом';
+
+?>
